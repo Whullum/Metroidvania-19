a5e7af006a9fe082d103de1358542b5bbbdaf7bb
Will <55513273+Whullum@users.noreply.github.com>
2023-03-02 17:55:39 -0500

    created rough outlines for stage 1

--- Metroidvania 19/Assets/Scripts/Map/Door.cs
+++ Metroidvania 19/Assets/Scripts/Map/Door.cs
@@ -18,6 +18,7 @@ public class Door : InteractableObject
     [SerializeField] private MapNode parentMapNode;
     [SerializeField] private MapManager mapManager;
     [SerializeField] private DoorDirection playerSpawnDirection;
+    [SerializeField] private const int playerSpawnDistance = 5;
 
     public int DoorNumber { get => doorNumber; set => doorNumber = value; }
 
@@ -34,32 +35,45 @@ public class Door : InteractableObject
         if (collision.gameObject.tag == "Player")
         {
             // Access the correct MapNode through the mapLevels dictionary
-            MapNode connectingNode = mapManager.CurrentMap.mapLevels[connectingMapName];
+            MapNode connectingNode = null;
 
-            Instantiate(connectingNode.gameObject);
-
-            // Spawn the player in the correct direction
-            Vector3 startingDisplacement = new Vector3();
-            switch (playerSpawnDirection)
+            if (mapManager.CurrentMap.mapLevels[connectingMapName] != null)
             {
-                // Doors are rotated, so use the door's local X axis to get the correct starting direction
-                case DoorDirection.NORTH:
-                case DoorDirection.WEST:
-                    startingDisplacement = new Vector3(3, 0);
-                    break;
-                case DoorDirection.SOUTH:
-                case DoorDirection.EAST:
-                    startingDisplacement = new Vector3(-3, 0);
-                    break;
+                connectingNode = mapManager.CurrentMap.mapLevels[connectingMapName];
+
+                Instantiate(connectingNode.gameObject);
+
+                // Spawn the player in the correct direction
+                Vector3 startingDisplacement = new Vector3();
+                switch (playerSpawnDirection)
+                {
+                    // Doors are rotated, so use the door's local X axis to get the correct starting direction
+                    case DoorDirection.NORTH:
+                        startingDisplacement = new Vector3(0, playerSpawnDistance);
+                        break;
+                    case DoorDirection.EAST:
+                        startingDisplacement = new Vector3(-playerSpawnDistance, 0);
+                        break;
+                    case DoorDirection.SOUTH:
+                        startingDisplacement = new Vector3(0, -playerSpawnDistance);
+                        break;
+                    case DoorDirection.WEST:
+                        startingDisplacement = new Vector3(playerSpawnDistance, 0);
+                        break;
+                }
+
+                // Recenter the minimap to the correct location
+                mapManager.RecenterMinimap(playerSpawnDirection);
+
+                // Spawn the player in front of the door with the matching doorNumber in the connected level/node
+                Door connectingDoor = connectingNode.GetConnectingDoorNumber(this.doorNumber);
+                collision.gameObject.transform.position = connectingDoor.transform.position + startingDisplacement;
+                Destroy(this.parentMapNode.gameObject);
             }
-
-            // Recenter the minimap to the correct location
-            mapManager.RecenterMinimap(playerSpawnDirection);
-
-            // Spawn the player in front of the door with the matching doorNumber in the connected level/node
-            Door connectingDoor = connectingNode.GetConnectingDoorNumber(this.doorNumber);
-            collision.gameObject.transform.position = connectingDoor.transform.position + startingDisplacement;
-            Destroy(this.parentMapNode.gameObject);
+            else
+            {
+                Debug.LogError("Warning: No map level with the name of " + connectingMapName + " located in mapLevels dictionary.");
+            }
         }
     }
 
