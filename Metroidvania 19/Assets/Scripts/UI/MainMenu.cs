eda14ad4c5d1a19a6fdcfbedcca5cc6f5618e8e9
Will <55513273+Whullum@users.noreply.github.com>
2023-03-11 16:35:26 -0500

    changed main scene and implemented ability powerups

--- Metroidvania 19/Assets/Scripts/UI/MainMenu.cs
+++ Metroidvania 19/Assets/Scripts/UI/MainMenu.cs
@@ -38,7 +38,7 @@ public class MainMenu : MonoBehaviour
          menuCanvasForeground.SetActive(true);
         LeanTween.alpha(menuCanvasForeground.GetComponent<RectTransform>(), 1f, 3f);
         yield return new WaitForSeconds(3f);
-        SceneManager.LoadScene("Stage1_Containment");
+        SceneManager.LoadScene("Main");
     }
 
     public void ShowSettings() {
