75a7d3267741dee936707f29a708974dc168eb17
Will Bermudez <willbermudez.personal@gmail.com>
2023-03-16 21:37:21 -0400

    Meat piece fix part 1

--- Metroidvania 19/Assets/EndGame.cs
+++ Metroidvania 19/Assets/EndGame.cs
@@ -8,10 +8,16 @@ public class EndGame : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        
         if (collision.gameObject.tag == "Player")
         {
+            foreach (PlayerHealthRestore meat in GameObject.FindObjectsOfType<PlayerHealthRestore>()) {
+                Destroy(meat.gameObject);
+            }
             MusicManager.instance.StopMusic();
             SceneManager.LoadScene("Start Menu");
         }
+
+        
     }
 }
